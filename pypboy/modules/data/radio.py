2808005ab8ce74d0d7fd71eab0e61a532c7d72c8
ratchedgears <ajvinke@gmail.com>
2013-08-26 00:51:25 +0200

    added select able stations
    create folders gnr and enc in sounds->radio
    place the music in them and it will play the songs

--- pypboy/modules/data/radio.py
+++ pypboy/modules/data/radio.py
@@ -1,6 +1,7 @@
 import pypboy
 import config
-
+import game
+import pypboy.ui
 from pypboy.modules.data import entities
 
 class Module(pypboy.SubModule):
@@ -10,15 +11,33 @@ class Module(pypboy.SubModule):
 	def __init__(self, *args, **kwargs):
 		super(Module, self).__init__(*args, **kwargs)
 		self.stations = [
-			entities.GalaxyNewsRadio()
+			entities.GalaxyNewsRadio(),
+			entities.RadioEnclave()
 		]
 		for station in self.stations:
 			self.add(station)
 		self.active_station = None
+		self.menu = pypboy.ui.Menu(300, ["Off", "Galaxy News Radio", "Radio Enclave"], [self.stop_radio, self.play_gnr, self.play_enc], 0)
+		self.menu.rect[0] = 4
+		self.menu.rect[1] = 60
 		config.radio = self
-
+		self.add(self.menu)
+		
+		
+	def stop_radio(self):
+		print "Off"
+		if hasattr(self, 'active_station') and self.active_station:
+			self.active_station.stop()
+		
+	def play_gnr(self):
+		print "GNR"
 		self.select_station(0)
+		self.active_station.render()
 
+	def play_enc(self):
+		print "Enclave"
+		self.select_station(1)
+		
 	def select_station(self, station):
 		if hasattr(self, 'active_station') and self.active_station:
 			self.active_station.stop()
